5f153bdc9e560a293acb20f7081620eebfe8d214
ClassicMC <classicmclive@gmail.com>
2022-05-27 10:56:09 -0400

    Added TextBox systems
    Changelog
    -New text box system
    -Image for text boxes added
    -Reformatted hitbox system 
    -Included Secondary hitbox for NPC's

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 const es = new Es(720,480);
 let images = {leaf:document.getElementById("leaf"),mud:document.getElementById("mud"),tree:document.getElementById("tree"),
     bg:document.getElementById("bg"),grass:document.getElementById("grass"),npc1:document.getElementById("npc1"),
-    title:document.getElementById("title")}
+    title:document.getElementById("title"),textbox:document.getElementById("textbox")}
 
 let key = {leftKeyPresed:false,rightKeyPressed:false,upKeyPressed:false,downKeyPressed:false,spaceKeyPressed:false}  
-let project = {scene:0,tOPC:1,textBlink:0,lastK:"left",ang:0,rotate:1,rotateAddition:0}
+let project = {scene:0,tOPC:1,textBlink:0,lastK:"left",ang:0,rotate:1,rotateAddition:0,tbs:0}
 let cameraPOS = {x:0,y:0}
 
 let keyUpdate = setInterval(keyInter,1);
@@ -12,7 +12,7 @@ let slowAnim = setInterval(blink,500);
 
 function keyInter(){
     document.addEventListener('keydown',keyPressed);
-    document.addEventListener('keyup',keyReleased);
+    document.addEventListener('keyup',keyReleased); 
     playerMove();
     if(project.lastK = "right" && checkAllCol()){cameraPOS.x ++;}
     if(project.lastK = "left" &&checkAllCol()){cameraPOS.x -=2;}
@@ -29,14 +29,12 @@ function drawLeaf(){
 }
 function playerMove(){
     if(key.spaceKeyPressed&&project.scene == 0){project.scene = 1;clearInterval(slowAnim);es.clear()}
+    if(key.spaceKeyPressed && es.checkCollisions(720/2-25,480/2-25,50,50,200-cameraPOS.x+55,300-cameraPOS.y+20,55,110)){project.tbs = 1;}
     else if(key.rightKeyPressed){move("right",.5,1,0);}
     else if(key.leftKeyPresed){move("left",-0.75,-1,0);}
     else if(key.upKeyPressed){move("up",.5,0,-1)}
     else if(key.downKeyPressed){move("down",-.5,0,1)}
-    else{
-        project.rotate = 0;
-        project.rotateAddition = 0;
-    }
+    else{project.rotate = 0;project.rotateAddition = 0;}
 }
 function move(keyLast,rotate,camX,camY){
     project.lastK = keyLast;
@@ -66,16 +64,14 @@ function keyReleased(evt){
 function checkAllCol(){if(es.checkCollisions(720/2-25,480/2-25,50,50,200-cameraPOS.x+60,300-cameraPOS.y+25,45,100)){return true;};if(es.checkCollisions(720/2-25,480/2-25,50,50,-20-cameraPOS.x,-20-cameraPOS.y,20,520)||es.checkCollisions(720/2-25,480/2-25,50,50,-20-cameraPOS.x,-20-cameraPOS.y,760,20)||es.checkCollisions(720/2-25,480/2-25,50,50,-20-cameraPOS.x,480-cameraPOS.y,760,20)||es.checkCollisions(720/2-25,480/2-25,50,50,720-cameraPOS.x,0-cameraPOS.y,-20,480||es.checkCollisions(720/2-25,480/2-25,50,50,-20-cameraPOS.x,-20-cameraPOS.y,20,520))){return true;}}
 function redraw(){
     c.save()
-    c.fillStyle = "#996633";
-    c.fillRect(0,0,720,480);
     if(project.scene == 0){     
-    es.background("#996633");
-    es.image(images.bg,0,0,720,480,0.15);
-    es.image(images.tree,150-40,100,150,150,0.3);
-    es.image(images.tree,150-10,100-50,150,150,0.3);
-    es.image(images.mud,400,300,300,200,0.5);
-    es.image(images.title,190,50,360,300,project.tOPC);
-    if(project.textBlink==0){es.text("Space to start",265,400,"#000000",0.3);}
+        es.background("#996633");
+        es.image(images.bg,0,0,720,480,0.15);
+        es.image(images.tree,150-40,100,150,150,0.3);
+        es.image(images.tree,150-10,100-50,150,150,0.3);
+        es.image(images.mud,400,300,300,200,0.5);
+        es.image(images.title,190,50,360,300,project.tOPC);
+        if(project.textBlink==0){es.text("Space to start",265,400,"#000000",0.3);}
     }
     else{
         es.background("#996633");
@@ -84,10 +80,12 @@ function redraw(){
         es.image(images.grass,0-cameraPOS.x,0-cameraPOS.y,720,480,0.1);
         es.image(images.mud,300-cameraPOS.x,300-cameraPOS.y,300,200);
         es.image(images.tree,150-cameraPOS.x-40,100-cameraPOS.y,150,150);
-        //The long row of hitboxes [P,WL,WU,WD,WR,NPC1]
-        //es.drawHitbox(720/2-25,480/2-25,50,50);es.drawHitbox(-20-cameraPOS.x,-20-cameraPOS.y,20,520);es.drawHitbox(-20-cameraPOS.x,-20-cameraPOS.y,760,20);es.drawHitbox(-20-cameraPOS.x,480-cameraPOS.y,760,20);es.drawHitbox(740-cameraPOS.x,0-cameraPOS.y,-20,480);es.drawHitbox(200-cameraPOS.x+60,300-cameraPOS.y+25,45,100);
+        //The long row of hitboxes [P,WL,WU,WD,WR,NPC1,NPC1TALK]
+        //es.drawHitbox(720/2-25,480/2-25,50,50);es.drawHitbox(-20-cameraPOS.x,-20-cameraPOS.y,20,520);es.drawHitbox(-20-cameraPOS.x,-20-cameraPOS.y,760,20);es.drawHitbox(-20-cameraPOS.x,480-cameraPOS.y,760,20);es.drawHitbox(740-cameraPOS.x,0-cameraPOS.y,-20,480);es.drawHitbox(200-cameraPOS.x+60,300-cameraPOS.y+25,45,100);es.drawHitbox(200-cameraPOS.x+55,300-cameraPOS.y+20,55,110);
         new Npc(200,300);
         drawLeaf();
+        if(es.checkCollisions(720/2-25,480/2-25,50,50,200-cameraPOS.x+55,300-cameraPOS.y+20,55,110)&&project.tbs == 1){es.image(images.textbox,10,290,350,200);es.text("Where am I",100,390,"#4d2600");es.text("???:",100,370,"#4d2600");}
+        else{project.tbs = 0}
     }
 }
 window.main = function(){
